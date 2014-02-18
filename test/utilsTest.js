3f219a6fa4892e29218d669e3ae4300b34a8ebe0
J Ancient German Spear <999@999.com>
2014-02-18 12:24:40 -0500

    more efficient testing of string to element

--- test/utilsTest.js
+++ test/utilsTest.js
@@ -5,8 +5,7 @@ var utils = require("../src/utils.js");
 describe('Utils', function(){
     describe('string to elements', function(){
         it('should return {H: 2, O: 1} from "H2O"', function(){
-            assert.equal(1, utils.stringToElements("H2O")['O']);
-            assert.equal(2, utils.stringToElements("H2O")['H']);
+            assert.deepEqual({H: 2, O: 1}, utils.stringToElements("H2O"));
         })
     });
     describe('split concat', function(){
