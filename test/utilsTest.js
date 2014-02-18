a9cb3bc70c841669be944e57ae8c5baae081fae7
J Ancient German Spear <999@999.com>
2014-02-18 12:11:10 -0500

    documentation for split concat

--- test/utilsTest.js
+++ test/utilsTest.js
@@ -8,5 +8,20 @@ describe('Utils', function(){
             assert.equal(1, utils.stringToElements("H2O")['O']);
             assert.equal(2, utils.stringToElements("H2O")['H']);
         })
+    });
+    describe('split concat', function(){
+        it('should return an array of arrays with the concatenated array split and added', function(){
+            var array1 = [1, 2, 3];
+            var array2 = [3, 4];
+            var expected = [ [1,2,3,3], [1,2,3,4] ];
+            var actual = utils.splitConcat(array1, array2);
+            assert.deepEqual(expected, actual);
+        });
+        it('should return an array split into single element arrays when concatenated []', function(){
+            var input = [1, 2, 3];
+            var expected = [ [1], [2], [3] ];
+            var actual = utils.splitConcat([], input);
+            assert.deepEqual(expected, actual);
+        })
     })
 });
